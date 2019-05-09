52454bdba28d49443a5d72239e9e672f06a509a1
Savva Shishak <s.shishak@yandex.ru>
2019-05-09 22:16:57 +0500

    firs and last commit

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,71 @@
-
 <template>
+  <!--Не интересный шаблон-->
   <div id="app" class="container">
-    <label for="enter" :class="{ red: err, green: success}">Введите название поля от A1 до H8</label>
+    <label for="enter" :class="{ red: err, dis: success}">Введите название поля от A1 до H8</label>
     <div class="row">
     <input type="text" v-model="field" class="form-control col-1">
     <span :class="{dis: !err, 'col-4': true}">Такого поля нет</span>
     </div>
-    <p :class="{green: !success}">{{res}}</p>
+    <p :class="{dis: !success}">{{res}}</p>
   </div>
 </template>
 
 <script>
-
+// сам ход работы:
+// для того чтобы определять доступные для коня ходы я решил перевести все названия клеток в координаты
 export default {
   data () {
     return {
+      // для самой задачи
       field: '',
       res: '',
+      // для стилей
       err: false,
       success: false,
     }
   },
-  methods:{// функция возвращающая нужные нам значения
-    Result: function(num, symbol){
-      let fields = [];
+  watch: {// это валидация, чтобы не получить не существующие поля:
+    field: function() {
+      if (this.field.length > 2) {// проверка на количество знаков
+        this.Error();
+      } else {
+        this.err = false
+        if (this.field.length == 2) {// когда пройдена проверка на количество знаков, проверяем сами знаки
 
-      // две функции принимающие для преобразования буквы в координату и обратно
-      function number(sym){// делает из буквы координату
-        let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-        return letters.indexOf(sym) + 1
+          let symbol = this.field.toLowerCase().charAt(0),
+          num = this.field.charAt(1),
+          test1 = /[a-h]/,
+          test2 = /[1-8]/;
+          if((test1.test(symbol)) && (test2.test(num))){
+
+            this.Result(num, symbol);// при успешой валидации используем нужную функцию
+
+          } else {
+
+            this.Error();
+          };
+
+        } else {
+          this.res = ''
+          this.None()
+        }
       }
-      function letter(num){// делает из координаты букву
+    }
+  },
+  methods:{// функция показывающая результат
+    Result: function(num, symbol){
+      this.Success();// стилями говорим что всё успешно
+
+      let fields = [];// создаём локальный массив
+
+      // две функции принимающие для преобразования буквы в ординаты и обратно
+      function number(sym){
         let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-        if (num < 0 || num > 8) {
+        return letters.indexOf(sym) + 1// делает из буквы ординату
+      }
+      function letter(num){
+        let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        if (num < 0 || num > 8) {// делает из ординаты букву
           return ''
         }
         for(let i = 0; i < 8; i++){
@@ -42,6 +75,7 @@ export default {
         }
       }
 
+      // заполняем массив всеми координатами
       fields.push({x:(+num + 2), y:(letter(number(symbol) + 1))})
       fields.push({x:(+num + 2), y:(letter(number(symbol) - 1))})
       fields.push({x:(+num - 2), y:(letter(number(symbol) + 1))})
@@ -51,16 +85,19 @@ export default {
       fields.push({x:(+num - 1), y:(letter(number(symbol) + 2))})
       fields.push({x:(+num - 1), y:(letter(number(symbol) - 2))})
       
+      // фильтруем от невозможных координат
       fields = fields.filter(field => {
         if (field.x > 0 && field.x != NaN && field.x < 8 && field.y != undefined && field.y != '') {
           return true
         }
       })
 
+      // делает массив с обычными названиями координат
       let result = fields.map(item => {
         return item.y + item.x
       })
 
+      // вот и всё!
       this.res = 'Конь может сходить на: ' + result.join(', ');
     },
 
@@ -80,42 +117,12 @@ export default {
       this.err = false;
       this.success = false;
     }
-  },
-  watch: {
-    // это валидация:
-    field: function() {
-      if (this.field.length > 2) {// проверка на количество знаков
-        this.Error();
-      } else {
-        this.err = false
-        if (this.field.length == 2) {// когда пройдена проверка на количество знаков, проверяем сами знаки
-
-          let symbol = this.field.toLowerCase().charAt(0),
-          num = this.field.charAt(1),
-          test1 = /[a-h]/,
-          test2 = /[1-8]/;
-          if((test1.test(symbol)) && (test2.test(num))){
-
-            this.res = this.field;
-            this.Success();
-            this.Result(num, symbol);// при успешой валидации используем нужную функцию
-
-          } else {
-
-            this.Error();
-          };
-
-        } else {
-          this.res = ''
-          this.None()
-        }
-      }
-    }
   }
 }
 </script>
 
 <style lang="scss">
+// не много стилей для красоты
 *{
   transition: .3s
 }
@@ -129,14 +136,8 @@ export default {
       color: red;
       border-color: red; 
     }
-    .green{
-      color: green;
-      border-color: green;
-      transform: scale(0)
-    }
     .dis{
-      transform: scale(0);
-      overflow: hidden;
+      transform: scale(0)
     }
   }
 </style>
